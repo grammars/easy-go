b30f25f007540e896c7a6cc7d604eb2637f1de0d
gjk <278884301@qq.com>
2024-07-09 23:42:56 +0800

    mmmm

--- cmd/easy_entry.go
+++ cmd/easy_entry.go
@@ -2,68 +2,13 @@ package main
 
 import (
 	"fmt"
-	"github.com/gorilla/websocket"
-	"log"
-	"net/url"
-	"os"
-	"os/signal"
 	"time"
 )
 
 func main() {
 	fmt.Println("Hello Gopher!")
-
-	interrupt := make(chan os.Signal, 1)
-	signal.Notify(interrupt, os.Interrupt)
-
-	u := url.URL{Scheme: "wss", Host: "dev.ydwlgame.com:443", Path: "/wocao"}
-	log.Printf("connecting to %s", u.String())
-
-	c, _, err := websocket.DefaultDialer.Dial(u.String(), nil)
-	if err != nil {
-		log.Fatal("dial:", err)
+	for i := 1; i <= 500; i++ {
+		go PlayRawSocket(i)
 	}
-	defer c.Close()
-
-	done := make(chan struct{})
-
-	go func() {
-		defer close(done)
-		for {
-			_, message, err := c.ReadMessage()
-			if err != nil {
-				log.Println("read:", err)
-				return
-			}
-			log.Printf("recv: %s", message)
-		}
-	}()
-
-	ticker := time.NewTicker(time.Second)
-	defer ticker.Stop()
-
-	for {
-		select {
-		case <-done:
-			return
-		case t := <-ticker.C:
-			err := c.WriteMessage(websocket.BinaryMessage, []byte(t.String()))
-			if err != nil {
-				log.Println("write:", err)
-				return
-			}
-		case <-interrupt:
-			log.Println("interrupt")
-			err := c.WriteMessage(websocket.CloseMessage, websocket.FormatCloseMessage(websocket.CloseNormalClosure, ""))
-			if err != nil {
-				log.Println("write close:", err)
-				return
-			}
-			select {
-			case <-done:
-			case <-time.After(time.Second):
-			}
-			return
-		}
-	}
+	time.Sleep(60 * time.Second)
 }
--- cmd/raw_sock.go
+++ cmd/raw_sock.go
@@ -6,11 +6,11 @@ import (
 	"time"
 )
 
-func PlayRawSocket() {
-	fmt.Println("PlayRawSocket begin!")
+func PlayRawSocket(index int) {
+	fmt.Printf("PlayRawSocket %d begin!\n", index)
 	conn, err := net.Dial("tcp", "localhost:1888")
 	if err != nil {
-		fmt.Printf("创建连接失败，错误:%v\n", err)
+		fmt.Printf("[%d]创建连接失败，错误:%v\n", index, err)
 		return
 	}
 	defer func(conn net.Conn) {
@@ -19,23 +19,25 @@ func PlayRawSocket() {
 			fmt.Printf("连接关闭失败，错误:%v\n\n", err)
 		}
 	}(conn)
-	fmt.Printf("连接服务端成功:%v\n", conn.RemoteAddr())
+	fmt.Printf("%d连接服务端成功:%v\n", index, conn.RemoteAddr())
 
-	go func() {
-		_, err := conn.Write([]byte("hello world"))
+	for i := 0; i < 100; i++ {
+		_, err := conn.Write([]byte(fmt.Sprintf("#%d#hello world(%d)", index, i)))
 		if err != nil {
 			fmt.Printf("发送消息失败:%v\n", err)
 			return
 		}
-		time.Sleep(10 * time.Millisecond)
+		time.Sleep(5000 * time.Millisecond)
 		var buf [1024]byte
 		n, err := conn.Read(buf[:])
 		if err != nil {
 			fmt.Printf("read failed, err:%v\n", err)
 			return
 		}
-		fmt.Println("收到服务端回复,", string(buf[:n]))
-	}()
-
-	time.Sleep(10 * time.Second)
+		if n > 0 {
+			fmt.Printf("#%d# 收到服务端回复:%s\n", index, string(buf[:n]))
+		} else {
+			fmt.Printf("#%d# 没有收到服务端回复:\n")
+		}
+	}
 }
