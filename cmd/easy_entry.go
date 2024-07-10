a9018a359ad902cde61887b4aa987d0c3bf4a226
gjk <278884301@qq.com>
2024-07-10 23:39:53 +0800

    sth

--- cmd/easy_entry.go
+++ cmd/easy_entry.go
@@ -2,13 +2,8 @@ package main
 
 import (
 	"fmt"
-	"time"
 )
 
 func main() {
 	fmt.Println("Hello Gopher!")
-	for i := 1; i <= 500; i++ {
-		go PlayRawSocket(i)
-	}
-	time.Sleep(60 * time.Second)
 }
